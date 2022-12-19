d7561e3de5561731344637fbefa099af48c78e53
Evgeny <evolenko@inbox.ru>
2022-12-19 23:44:51 +0300

    Added Task063 and Task065

---
+++ Task063CW_Nat_1_to_ N/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Clear();
+
+Console.Write("Введите натуральное число N: ");
+int n = EnterDigit();
+int start = 1;
+
+PrintNatural(n, start);
+
+void PrintNatural(int digit, int start)
+{
+    if (start <= digit)
+    {
+        Console.WriteLine(start);
+        PrintNatural(digit, start + 1);
+    }
+}
+
+int EnterDigit()
+{
+    int digit;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out digit)) ;
+        if (digit > 0) break;
+        else Console.Write("Ну просил же натуральное ЧИСЛО: ");
+    }
+    return digit;
+}
---
+++ Task065CW_Nat_M_to_N/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.Clear();
+
+Console.Write("Введите натуральное число M: ");
+int m = EnterDigit();
+
+Console.Write("Введите натуральное число N: ");
+int n = EnterDigit();
+
+PrintNatural(n, m);
+
+void PrintNatural(int digitN, int digitM)
+{
+    if (digitM <= digitN)
+    {
+        Console.WriteLine(digitM);
+        PrintNatural(digitN, digitM + 1);
+    }
+}
+
+int EnterDigit()
+{
+    int digit;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out digit)) ;
+        if (digit > 0) break;
+        else Console.Write("Ну просил же натуральное ЧИСЛО: ");
+    }
+    return digit;
+}
